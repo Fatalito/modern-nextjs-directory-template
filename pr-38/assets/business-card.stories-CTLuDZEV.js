0fca8190699d5162930eab056b90f4f9d27ce321
Fatalito <Fatalito@users.noreply.github.com>
2026-02-28 16:32:36 +0000

    deploy: 2552374d838a10908c934b61374258ebf33bab8a

---
+++ pr-38/assets/business-card.stories-CTLuDZEV.js
@@ -0,0 +1,9 @@
+import{B as l,c as f}from"./business-card-BZHIOjrx.js";import"./iframe-CmWTCUyn.js";import"./preload-helper-PPVm8Dsz.js";function m(t,e,r){return e in t?Object.defineProperty(t,e,{value:r,enumerable:!0,configurable:!0,writable:!0}):t[e]=r,t}function o(t){for(var e=1;e<arguments.length;e++){var r=arguments[e]!=null?arguments[e]:{},n=Object.keys(r);typeof Object.getOwnPropertySymbols=="function"&&(n=n.concat(Object.getOwnPropertySymbols(r).filter(function(s){return Object.getOwnPropertyDescriptor(r,s).enumerable}))),n.forEach(function(s){m(t,s,r[s])})}return t}function O(t,e){var r=Object.keys(t);if(Object.getOwnPropertySymbols){var n=Object.getOwnPropertySymbols(t);r.push.apply(r,n)}return r}function u(t,e){return e=e??{},Object.getOwnPropertyDescriptors?Object.defineProperties(t,Object.getOwnPropertyDescriptors(e)):O(Object(e)).forEach(function(r){Object.defineProperty(t,r,Object.getOwnPropertyDescriptor(e,r))}),t}var i,p,c,g={component:l,title:"Entities/Business/BusinessCard"},a={args:{business:f({name:"Acme Coffee Shop",category:"hospitality",images:["https://images.unsplash.com/photo-1511920170033-f8396924c348?w=800"]})}};a.parameters=u(o({},a.parameters),{docs:u(o({},(i=a.parameters)===null||i===void 0?void 0:i.docs),{source:o({originalSource:`{
+  args: {
+    business: createBusiness({
+      name: "Acme Coffee Shop",
+      category: "hospitality",
+      images: ["https://images.unsplash.com/photo-1511920170033-f8396924c348?w=800"]
+    })
+  }
+}`},(c=a.parameters)===null||c===void 0||(p=c.docs)===null||p===void 0?void 0:p.source)})});const _=["Default"];export{a as Default,_ as __namedExportsOrder,g as default};
